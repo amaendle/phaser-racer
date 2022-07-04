491802cb92a8f7483e96b642b543c68ca5b9af82
amaendle <AndemWue@gmail.com>
2022-07-04 10:44:54 +0200

    initial upload

---
+++ engine2.js
@@ -0,0 +1,64 @@
+var startengine, stopengine, iniengine
+window.onload = function(){
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
+      xhr = new XMLHttpRequest(),
+      pitch = {step: 0, min: 1.1, max: 2.2},
+      source, intervalId;
+
+  function createPitchStep(n) {
+    return function(e) {
+      if (e.keyCode == 38) {
+        pitch.step = n;
+      }
+    }
+  }
+
+  xhr.open("GET", "assets/sounds/engine.wav", true);
+  xhr.responseType = "arraybuffer";
+  xhr.onload = function(e){
+    var audioData = this.response;
+
+    window.addEventListener("keydown", createPitchStep(0.02))
+    window.addEventListener("keyup", createPitchStep(-0.02))
+
+    iniengine = function(e){
+      if (!source) {
+        source = audioCtx.createBufferSource();
+
+        audioCtx.decodeAudioData(audioData, function(buffer) {
+          source.buffer = buffer;
+          source.loop = true;
+          source.connect(audioCtx.destination);
+          source.start();
+        });
+
+        intervalId = setInterval(function(){
+          var currPitch = source.playbackRate.value;
+
+          if ((pitch.step < 0 && currPitch > pitch.min) ||
+              (pitch.step > 0 && currPitch < pitch.max)) {
+            source.playbackRate.value += pitch.step;
+          }
+        }, 50);
+      }
+           audioCtx.suspend();
+    };
+    
+        startengine = function(e){
+      if (source) {
+     audioCtx.resume() ; 
+      }
+    };
+
+    stopengine = function(e){
+      if (source) {
+     //   source.stop();
+     //   source = null;
+     //   clearInterval(intervalId);
+     audioCtx.suspend();
+      }
+    };
+  };
+
+  xhr.send();
+};
